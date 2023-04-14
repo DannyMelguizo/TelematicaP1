c6703c4646396ed4561652791bb5271c7ab80452
DanyMelguizo <dmelguizor@eafit.edu.co>
2023-04-14 16:35:27 -0500

    'files'

--- api/Server.py
+++ api/Server.py
@@ -110,15 +110,13 @@ def server_execution():
     tuple_connection = (server_address,constants.PORT)
     server_socket.bind(tuple_connection)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print ('Socket is bind to address and port...')
+    print ('Socket is bind to address and port...') 
     server_socket.listen(5)
     print('Socket is listening...')
     while True:
         client_connection, client_address = server_socket.accept()
         client_thread = threading.Thread(target=handler_client_connection, args=(client_connection,client_address))
         client_thread.start()
-    print('Socket is closed...')
-    server_socket.close()
 
 if __name__ == "__main__":
     main()
